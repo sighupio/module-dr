d4b8e669d554a754caf3f84d07b64ee38ba416bd
Marco Paggioro <marco.paggioro@gmail.com>
2026-09-16 14:26:53 +0200

    feat: add `v1` version for VolumeGroupSnapshot and VolumeGroupSnapshotContent CRDs

--- katalog/tests/helper.bash
+++ katalog/tests/helper.bash
@@ -1,3 +1,7 @@
+# Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 #!/usr/bin/env bats
 
 # shellcheck disable=SC2086,SC2154,SC2034
